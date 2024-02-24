4fbc32d28c464c7f0881f5c1d2896f95f7d974a0
ealhl <yayt.tuk@gmail.com>
2024-02-24 11:05:42 -0800

    refactor add user to sql

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -50,10 +50,18 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+    .query(
+      `INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING *`,
+      [user.name, user.email, user.password]
+    )
+    .then((result) => {
+      console.log("add user", result.rows[0]);
+      return result.rows[0];
+    })
+    .catch((err) => {
+      console.log("err", err.message);
+    });
 };
 
 /// Reservations
